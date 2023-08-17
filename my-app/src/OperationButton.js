2b2cd5b5f4e7ba0ad3f2d218453df2a49acdb868
TheMaddoxJ <maddoxjohnson7@gmail.com>
2023-08-17 16:22:49 -0400

    functionality of all buttons working

---
+++ my-app/src/OperationButton.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App";
+
+export default function OperationButtonButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
